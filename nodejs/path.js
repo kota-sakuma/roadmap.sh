2e483ab7d4c98dd06cecf47cec133cc424bbc64f
kota-sakuma <kotasakuma98@gmail.com>
2026-01-18 14:50:26 +0900

    fix(node.js): modify the way to use __dirname

--- nodejs/path.js
+++ nodejs/path.js
@@ -1,7 +1,12 @@
+import { fileURLToPath } from 'node:url';
 import fs from 'node:fs/promises';
 import path from 'node:path';
 
 // スクリプトと同じ階層にある 'package.json' を安全に指定
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const basename = path.basename(__filename);
+console.log(basename);
 const targetPath = path.join(__dirname, 'package.json');
 
 async function loadConfig() {
