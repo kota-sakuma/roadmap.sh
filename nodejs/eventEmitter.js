e47042eb3a3c87f4126e4ab2b1a4c69d8af9a718
kota-sakuma <kotasakuma98@gmail.com>
2026-01-18 13:23:48 +0900

    feat(node.js): add event emitter

---
+++ nodejs/eventEmitter.js
@@ -0,0 +1,20 @@
+import { EventEmitter } from 'events';
+
+const myEmitter = new EventEmitter();
+
+// 1. リスナーの登録 (何度でも反応する)
+myEmitter.on('welcome', (name) => {
+    console.log(`こんにちは、${name}さん！`);
+});
+
+// 2. リスナーの登録 (1回だけ反応する)
+myEmitter.once('login', () => {
+    console.log('初めてのログインを確認しました。');
+});
+
+// 3. イベントの発行
+myEmitter.emit('welcome', '田中'); // 田中さん！
+myEmitter.emit('welcome', '佐藤'); // 佐藤さん！
+
+myEmitter.emit('login'); // 「初めての...」が表示される
+myEmitter.emit('login'); // 2回目なので何も起きない
