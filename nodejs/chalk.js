ce1b508ca6809dcf84785ba6c0d00d361fb282db
kota-sakuma <sakuma.kota@cloud-ace.jp>
2026-01-20 11:16:45 +0900

    feat(node.js): add chalk

---
+++ nodejs/chalk.js
@@ -0,0 +1,5 @@
+import chalk from 'chalk';
+
+console.log(chalk.blue('Hello') + ' World' + chalk.red('!'));
+console.log(chalk.bold.bgGreen.black(' SUCCESS ') + 'Complete the process');
+console.log(chalk.hex('#FFA500')('Warning: This is a warning!'));
