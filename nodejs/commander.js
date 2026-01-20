c08aa72355a74eddd7da32cf0ef02ebf4db36f07
kota-sakuma <sakuma.kota@cloud-ace.jp>
2026-01-20 11:57:52 +0900

    feat(node.js): add commander

---
+++ nodejs/commander.js
@@ -0,0 +1,22 @@
+import { Command } from 'commander';
+
+const program = new Command();
+
+program
+  .name('my-cli')
+  .description('My CLI Application')
+  .version('1.0.0');
+
+program
+  .argument('<name>', 'User name')
+  .option('-p, --port <number>', 'Port number', '3000')
+  .option('-d, --debug', 'Enable debug mode')
+  .action((name, options) => {
+    console.log(`Hello, ${name}!`);
+    console.log(`Port: ${options.port}`);
+    if (options.debug) {
+      console.log('Debug mode is enabled');
+    }
+  });
+
+program.parse(process.argv);
